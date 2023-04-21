289adeffc304feb96ad2b2ff25f25dc246396256
mrt <m8mrt38@gmail.com>
2023-04-21 13:57:38 +0330

    URI problem of Auth tests solved

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -18,7 +18,7 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request)//: Response
+    public function store(Request $request): Response
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
@@ -36,7 +36,7 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        //return response()->noContent();
-        return response()->json($user);
+        return response()->noContent();
+        // return response()->json($user);
     }
 }
